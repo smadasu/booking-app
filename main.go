951308fc76b3ba06d63a98092946a930c9ffb7a7
Sreenath Madasu <MADASUSX@legal.regn.net>
2021-12-25 10:47:17 -0500

    Simple Array

--- main.go
+++ main.go
@@ -21,7 +21,13 @@ func main() {
 	fmt.Scan(&emailAddress)
 	fmt.Println("Please enter the number of tickets needed")
 	fmt.Scan(&userTickets)
-	fmt.Printf("Thank you %v %v for booking  %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, emailAddress, userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
 	remainingTickets = totalTickets - userTickets
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	var bookings [50] string
+	bookings[0] = firstName + " " + lastName
+	fmt.Printf("Array = %v\n", bookings)
+	fmt.Printf("First Value = %v\n", bookings[0])
+	fmt.Printf("Array Type = %T\n", bookings)
+	fmt.Printf("Array Length= %v\n", len(bookings))
 }
