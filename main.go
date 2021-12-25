4c625386423da0daaf4ab18ee0080aa09b56fd57
Sreenath Madasu <MADASUSX@legal.regn.net>
2021-12-25 11:42:24 -0500

    If else logic

--- main.go
+++ main.go
@@ -14,26 +14,35 @@ func main() {
 	firstNames := []string{}
 	for {
 		fmt.Printf("We have total of %v tickets and are %v still available\n",totalTickets, remainingTickets)
-		var firstName string
-		var lastName string
-		var emailAddress string
 		var userTickets uint
-		fmt.Println("Please enter your first name")
-		fmt.Scan(&firstName)
-		fmt.Println("Please enter your last name")
-		fmt.Scan(&lastName)
-		fmt.Println("Please enter your email address")
-		fmt.Scan(&emailAddress)
 		fmt.Println("Please enter the number of tickets needed")
 		fmt.Scan(&userTickets)
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
-		remainingTickets = remainingTickets - userTickets
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-		var bookings [] string
-		bookings = append(bookings, firstName + " " + lastName)
-		for _, booking := range bookings {
-			firstNames = append(firstNames, strings.Fields(booking)[0])
+		if userTickets <= remainingTickets {
+			var firstName string
+			var lastName string
+			var emailAddress string
+			fmt.Println("Please enter your first name")
+			fmt.Scan(&firstName)
+			fmt.Println("Please enter your last name")
+			fmt.Scan(&lastName)
+			fmt.Println("Please enter your email address")
+			fmt.Scan(&emailAddress)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
+			remainingTickets = remainingTickets - userTickets
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+			var bookings [] string
+			bookings = append(bookings, firstName + " " + lastName)
+			for _, booking := range bookings {
+				firstNames = append(firstNames, strings.Fields(booking)[0])
+			}
+			fmt.Printf("The first names of bookings are = %v\n", firstNames)
+			if remainingTickets == 0 {
+				fmt.Println("Conference is sold out")
+				break
+			}
+		} else {
+			fmt.Printf("We only have %v tckets remaining. Please enter a value less than or equal to that\n", remainingTickets)
+			continue
 		}
-		fmt.Printf("The first names of bookings are = %v\n", firstNames)
 	}
 }
