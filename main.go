db5fb0edf5d2eeb9624e86ba9513972f237b3a76
Sreenath Madasu <MADASUSX@legal.regn.net>
2021-12-28 16:28:46 -0500

    Using Go routine with wait group

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"booking-app/helper"
 	"fmt"
 	"time"
+	"sync"
 )
 
 var conferenceName = "Go Conference"
@@ -11,6 +12,7 @@ const totalTickets = 50
 var remainingTickets uint = 50
 var firstNames = []string{}
 var bookings []UserData
+var wg = sync.WaitGroup{}
 
 type UserData struct {
 	firstName string
@@ -21,34 +23,33 @@ type UserData struct {
 	
 func main() {
 	greetUsers()
-	for {
-		userTickets, firstName, lastName, emailAddress := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, emailAddress, userTickets, remainingTickets)
-		if !isValidName {
-			fmt.Println("Firstname or Last Name should have more than 2 characters")
-		} else if !isValidEmail {
-			fmt.Println("Invalid Email Address")
-		} else if !isValidTicketNumber {
-			fmt.Printf("required tickets %v cannot be more than remaining %v tickets\n", userTickets, remainingTickets)
-		} else {
-			var userData = UserData {
-				firstName: firstName,
-				lastName: lastName,
-				emailAddress: emailAddress,
-				userTickets: userTickets,
-			}
-			bookTickets(userData)
-			if remainingTickets == 0 {
-				fmt.Println("Conference is sold out")
-				break
-			}
+	userTickets, firstName, lastName, emailAddress := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, emailAddress, userTickets, remainingTickets)
+	if !isValidName {
+		fmt.Println("Firstname or Last Name should have more than 2 characters")
+	} else if !isValidEmail {
+		fmt.Println("Invalid Email Address")
+	} else if !isValidTicketNumber {
+		fmt.Printf("required tickets %v cannot be more than remaining %v tickets\n", userTickets, remainingTickets)
+	} else {
+		var userData = UserData {
+			firstName: firstName,
+			lastName: lastName,
+			emailAddress: emailAddress,
+			userTickets: userTickets,
+		}
+		bookTickets(userData)
+		if remainingTickets == 0 {
+			fmt.Println("Conference is sold out")
 		}
 	}
+	wg.Wait()
 }
 
 func bookTickets(userData UserData) {
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", 
 				userData.firstName, userData.lastName, userData.userTickets, userData.emailAddress)
+	wg.Add(1)
 	go sendTicket(userData)
 	remainingTickets = remainingTickets - userData.userTickets
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
@@ -87,9 +88,10 @@ func greetUsers() {
 }
 
 func sendTicket(userData UserData) {
-	time.Sleep(10 * time.Second)
+	time.Sleep(20 * time.Second)
 	var ticket = fmt.Sprintf("%v tickets for %v %v", userData.userTickets, userData.firstName, userData.lastName)
 	fmt.Println("*****************")
 	fmt.Printf("Sending ticket:\n %v \n to email address %v\n", ticket, userData.emailAddress)
 	fmt.Println("*****************")
+	wg.Done()
 }
